1be200c1e036ff428b61b7246a10f475d007990f
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-01 08:12:46 +0000

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,22 +3,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints single digits of base 10 starting from 0
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n = 0;
-	
+	int n;
 
 	for (n = 0; n <= 9; n++)
 	{
-	    printf("%d",n);
+		printf("%d", n);
 	}
-
 	printf("\n");
-
 
 	return (0);
 }
