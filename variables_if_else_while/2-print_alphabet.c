63f383d99f85811d3050bdac626879d232932c21
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-01 07:45:10 +0000

    task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,15 @@
  */
 int main(void)
 {
-    char c = 'a';
-    while (c <= 'z')
-    {
-        putchar(c);
-        c++;
-    }
-    putchar('\n');
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
 	return (0);
 }
 
