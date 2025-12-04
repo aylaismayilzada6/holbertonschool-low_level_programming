b96be25f0fc92bd7f9713bc9c0d47b69507d8929
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-04 09:41:01 +0000

    task 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -6,14 +6,11 @@
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-        if (c >= '0' && c <= '9')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
+
