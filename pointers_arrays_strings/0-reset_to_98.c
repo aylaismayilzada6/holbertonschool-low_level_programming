f1c16ba58488ffaeb4e009ad50acc1b44bfba0a0
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-08 09:18:44 +0000

    adding task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,11 @@
 /**
  * reset_to_98 - updates the value of the integer it points to to 98
  * @n: pointer to the integer to update
+ *
+ * Return: void
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
+
