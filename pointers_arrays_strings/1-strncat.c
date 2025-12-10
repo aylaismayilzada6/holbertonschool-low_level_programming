1eb301436db27188f1df19206e5dcebef88b7ade
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-10 06:55:03 +0000

    more ex, task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes from src
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to copy from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	/* Find the end of dest */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Copy up to n bytes from src */
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add final null terminator */
+	dest[i] = '\0';
+
+	return (dest);
+}
+
