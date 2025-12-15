946b6ddfe27bc95c7db6ef754cba805cc884f8df
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-15 10:48:36 +0000

    recursion / task 0

--- recursion/main.h
+++ recursion/main.h
@@ -10,5 +10,15 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
 
 #endif /* MAIN_H */
