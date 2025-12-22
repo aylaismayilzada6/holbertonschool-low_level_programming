7af97d0de35ecdadbf84fee06d2fbd75a908704e
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2025-12-22 10:56:31 +0000

    main.h

---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+this the readme for malloc and free
---
+++ malloc_free/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+#endif /* MAIN_H */
