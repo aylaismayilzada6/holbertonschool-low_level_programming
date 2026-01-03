fb1e7919726988719f341b31067cebd31017df3a
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-01-03 14:46:12 +0000

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,47 +1,74 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
-dog_t *new_dog(char *name, float age, char *owner)
+/**
+ * _strlen - finds the length of a string
+ * @s: string to check
+ * Return: length of s
+ */
+int _strlen(char *s)
 {
-    dog_t *d;
-    char *name_copy, *owner_copy;
+	int len = 0;
 
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return (NULL);
-
-    if (name != NULL)
-    {
-        name_copy = malloc(strlen(name) + 1);
-        if (name_copy == NULL)
-        {
-            free(d);
-            return (NULL);
-        }
-        strcpy(name_copy, name);
-    }
-    else
-        name_copy = NULL;
-
-    if (owner != NULL)
-    {
-        owner_copy = malloc(strlen(owner) + 1);
-        if (owner_copy == NULL)
-        {
-            free(name_copy);
-            free(d);
-            return (NULL);
-        }
-        strcpy(owner_copy, owner);
-    }
-    else
-        owner_copy = NULL;
-
-    d->name = name_copy;
-    d->age = age;
-    d->owner = owner_copy;
-
-    return (d);
+	while (s[len])
+		len++;
+	return (len);
 }
 
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: destination buffer
+ * @src: source string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to new dog_t, or NULL if fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	/* Allocate and copy name */
+	d->name = malloc(_strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	_strcpy(d->name, name);
+
+	/* Allocate and copy owner */
+	d->owner = malloc(_strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	_strcpy(d->owner, owner);
+
+	d->age = age;
+
+	return (d);
+}
