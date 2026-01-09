329390ecb5a6dee19f7fad8d56aa1d90f7a6986d
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-01-09 06:46:26 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,67 +1,90 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
- * struct printer - Struct to match format char with a function
- * @symbol: The format character (c, i, f, s)
- * @print: Pointer to the function that prints the type
+ * p_char - Prints a character
+ * @arg: A list of arguments pointing to the character to be printed
  */
-typedef struct printer
+void p_char(va_list arg)
 {
-    char *symbol;
-    void (*print)(va_list arg);
-} p_t;
-
-/* Helper functions to handle the printing */
-void p_char(va_list arg) { printf("%c", va_arg(arg, int)); }
-void p_int(va_list arg) { printf("%d", va_arg(arg, int)); }
-void p_float(va_list arg) { printf("%f", va_arg(arg, double)); }
-void p_string(va_list arg)
-{
-    char *str = va_arg(arg, char *);
-    if (!str)
-    {
-        printf("(nil)");
-        return;
-    }
-    printf("%s", str);
+	printf("%c", va_arg(arg, int));
 }
 
 /**
- * print_all - Prints anything based on format
- * @format: list of types of arguments
+ * p_int - Prints an integer
+ * @arg: A list of arguments pointing to the integer to be printed
+ */
+void p_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * p_float - Prints a float
+ * @arg: A list of arguments pointing to the float to be printed
+ */
+void p_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * p_string - Prints a string
+ * @arg: A list of arguments pointing to the string to be printed
+ */
+void p_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+	if (!str)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - Prints anything based on a format string
+ * @format: A list of types of arguments passed to the function
+ * @...: A variable number of arguments to be printed
+ *
+ * Description: If the string is NULL, (nil) is printed instead.
+ * Any character not in the format list is ignored.
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0, j = 0;
-    char *sep = "";
-    p_t funcs[] = {
-        {"c", p_char},
-        {"i", p_int},
-        {"f", p_float},
-        {"s", p_string},
-        {NULL, NULL}
-    };
+	va_list args;
+	int i = 0, j = 0;
+	char *sep = "";
+	p_t funcs[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_string},
+		{NULL, NULL}
+	};
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        j = 0;
-        while (funcs[j].symbol)
-        {
-            if (format[i] == *(funcs[j].symbol))
-            {
-                printf("%s", sep);
-                funcs[j].print(args);
-                sep = ", ";
-            }
-            j++;
-        }
-        i++;
-    }
+	while (format && format[i])
+	{
+		j = 0;
+		while (funcs[j].symbol)
+		{
+			if (format[i] == *(funcs[j].symbol))
+			{
+				printf("%s", sep);
+				funcs[j].print(args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
 
-    printf("\n");
-    va_end(args);
+	printf("\n");
+	va_end(args);
 }
