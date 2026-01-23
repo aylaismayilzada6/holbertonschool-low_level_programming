0ef8d01c309eee24782794fc3896ab18f4cefbe0
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-01-23 10:31:52 +0000

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output.
  * @filename: name of the file to read.
  * @letters: number of letters it should read and print.
  *
