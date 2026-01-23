7963d4c881df77e36365d2cb769d5be6a96ed496
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-01-23 10:54:11 +0000

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes text into it.
+ * @filename: the name of the file to create.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n_wrote, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* O_CREAT: create file, O_WRONLY: write only, O_TRUNC: empty existing */
+	/* 0600: permissions rw------- */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		n_wrote = write(fd, text_content, len);
+		if (n_wrote == -1 || n_wrote != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
